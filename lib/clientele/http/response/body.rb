beac7e3f8323fc35a94b8ea02d6df7847713529d
Chris Keele <dev@chriskeele.com>
2016-05-10 08:52:40 -0500

    New stuff in body.

--- lib/clientele/http/response/body.rb
+++ lib/clientele/http/response/body.rb
@@ -3,8 +3,33 @@ module Clientele
     class Response < RESPONSE
       class Body
 
-        def initialize(body)
-          @body = body
+        attr_accessor :raw, :parsed
+        def initialize(raw)
+          @raw = raw
+        end
+        
+        def parse
+          @parsed = if block_given?
+            yield @raw
+          elsif @parser
+            @parser.call @raw
+          else
+            raise "must supply a parser implementation to parse"
+          end
+        end
+        
+        def parsed?
+          !!@parsed
+        end
+        
+        def respond_to_missing?(method, priv = true)
+          parsed and parsed.respond_to? method, priv
+        end
+        
+        def method_missing(method, *args, **opts, &block)
+          if parsed and parsed.respond_to? method
+            parsed.send(method, *args, **opts, &block)
+          else; super; end
         end
 
       end
